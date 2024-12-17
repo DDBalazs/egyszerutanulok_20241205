9ae4b751aaccad8bdaf9a4843af1ba574362e783
Balázs Zsolt Csőke <csoke.balazs@verebelyszki.hu>
2024-12-17 12:26:32 +0100

    sada

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,8 +29,11 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.cbtelepules = new System.Windows.Forms.ComboBox();
+            this.dgadatok = new System.Windows.Forms.DataGridView();
+            this.nev = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.igazolvany = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.label1 = new System.Windows.Forms.Label();
+            this.cbtelepules = new System.Windows.Forms.ComboBox();
             this.rbigszam = new System.Windows.Forms.RadioButton();
             this.label2 = new System.Windows.Forms.Label();
             this.txigszam = new System.Windows.Forms.TextBox();
@@ -41,14 +44,11 @@
             this.label5 = new System.Windows.Forms.Label();
             this.dtszuletett = new System.Windows.Forms.DateTimePicker();
             this.label6 = new System.Windows.Forms.Label();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox5 = new System.Windows.Forms.TextBox();
-            this.textBox6 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.txirszam = new System.Windows.Forms.TextBox();
+            this.txvaros = new System.Windows.Forms.TextBox();
+            this.txutca = new System.Windows.Forms.TextBox();
+            this.btmod = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.dgadatok = new System.Windows.Forms.DataGridView();
-            this.nev = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.igazolvany = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgadatok)).BeginInit();
             this.SuspendLayout();
@@ -67,15 +67,32 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Válasszon települést!";
             // 
-            // cbtelepules
+            // dgadatok
             // 
-            this.cbtelepules.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.cbtelepules.FormattingEnabled = true;
-            this.cbtelepules.Location = new System.Drawing.Point(7, 40);
-            this.cbtelepules.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
-            this.cbtelepules.Name = "cbtelepules";
-            this.cbtelepules.Size = new System.Drawing.Size(406, 21);
-            this.cbtelepules.TabIndex = 0;
+            this.dgadatok.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgadatok.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.nev,
+            this.igazolvany});
+            this.dgadatok.Location = new System.Drawing.Point(20, 94);
+            this.dgadatok.Name = "dgadatok";
+            this.dgadatok.Size = new System.Drawing.Size(379, 262);
+            this.dgadatok.TabIndex = 2;
+            this.dgadatok.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgadatok_CellClick);
+            this.dgadatok.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgadatok_CellContentClick);
+            // 
+            // nev
+            // 
+            this.nev.HeaderText = "Név";
+            this.nev.Name = "nev";
+            this.nev.ReadOnly = true;
+            this.nev.Width = 150;
+            // 
+            // igazolvany
+            // 
+            this.igazolvany.HeaderText = "Igazolvány";
+            this.igazolvany.Name = "igazolvany";
+            this.igazolvany.ReadOnly = true;
+            this.igazolvany.Width = 150;
             // 
             // label1
             // 
@@ -86,6 +103,17 @@
             this.label1.Size = new System.Drawing.Size(225, 13);
             this.label1.TabIndex = 1;
             this.label1.Text = "Kattintson az elejére a kiválasztáshoz!";
+            // 
+            // cbtelepules
+            // 
+            this.cbtelepules.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbtelepules.FormattingEnabled = true;
+            this.cbtelepules.Location = new System.Drawing.Point(7, 40);
+            this.cbtelepules.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.cbtelepules.Name = "cbtelepules";
+            this.cbtelepules.Size = new System.Drawing.Size(406, 21);
+            this.cbtelepules.TabIndex = 0;
+            this.cbtelepules.SelectedIndexChanged += new System.EventHandler(this.cbtelepules_SelectedIndexChanged);
             // 
             // rbigszam
             // 
@@ -115,6 +143,7 @@
             this.txigszam.Name = "txigszam";
             this.txigszam.Size = new System.Drawing.Size(100, 20);
             this.txigszam.TabIndex = 3;
+            this.txigszam.TextChanged += new System.EventHandler(this.txigszam_TextChanged);
             // 
             // txosztaly
             // 
@@ -147,6 +176,7 @@
             this.txnev.Name = "txnev";
             this.txnev.Size = new System.Drawing.Size(230, 20);
             this.txnev.TabIndex = 7;
+            this.txnev.TextChanged += new System.EventHandler(this.txnev_TextChanged);
             // 
             // label5
             // 
@@ -175,35 +205,36 @@
             this.label6.TabIndex = 10;
             this.label6.Text = "Lakcím:";
             // 
-            // textBox4
+            // txirszam
             // 
-            this.textBox4.Location = new System.Drawing.Point(555, 156);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(100, 20);
-            this.textBox4.TabIndex = 11;
+            this.txirszam.Location = new System.Drawing.Point(555, 156);
+            this.txirszam.Name = "txirszam";
+            this.txirszam.Size = new System.Drawing.Size(100, 20);
+            this.txirszam.TabIndex = 11;
             // 
-            // textBox5
+            // txvaros
             // 
-            this.textBox5.Location = new System.Drawing.Point(555, 191);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(230, 20);
-            this.textBox5.TabIndex = 12;
+            this.txvaros.Location = new System.Drawing.Point(555, 191);
+            this.txvaros.Name = "txvaros";
+            this.txvaros.Size = new System.Drawing.Size(230, 20);
+            this.txvaros.TabIndex = 12;
             // 
-            // textBox6
+            // txutca
             // 
-            this.textBox6.Location = new System.Drawing.Point(555, 228);
-            this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(230, 20);
-            this.textBox6.TabIndex = 13;
+            this.txutca.Location = new System.Drawing.Point(555, 228);
+            this.txutca.Name = "txutca";
+            this.txutca.Size = new System.Drawing.Size(230, 20);
+            this.txutca.TabIndex = 13;
             // 
-            // button1
+            // btmod
             // 
-            this.button1.Location = new System.Drawing.Point(447, 264);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(338, 73);
-            this.button1.TabIndex = 14;
-            this.button1.Text = "Adatok módosítása";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btmod.Location = new System.Drawing.Point(447, 264);
+            this.btmod.Name = "btmod";
+            this.btmod.Size = new System.Drawing.Size(338, 73);
+            this.btmod.TabIndex = 14;
+            this.btmod.Text = "Adatok módosítása";
+            this.btmod.UseVisualStyleBackColor = true;
+            this.btmod.Click += new System.EventHandler(this.btmod_Click);
             // 
             // panel1
             // 
@@ -212,41 +243,16 @@
             this.panel1.Size = new System.Drawing.Size(341, 37);
             this.panel1.TabIndex = 15;
             // 
-            // dgadatok
-            // 
-            this.dgadatok.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgadatok.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.nev,
-            this.igazolvany});
-            this.dgadatok.Location = new System.Drawing.Point(20, 94);
-            this.dgadatok.Name = "dgadatok";
-            this.dgadatok.Size = new System.Drawing.Size(379, 262);
-            this.dgadatok.TabIndex = 2;
-            // 
-            // nev
-            // 
-            this.nev.HeaderText = "Név";
-            this.nev.Name = "nev";
-            this.nev.ReadOnly = true;
-            this.nev.Width = 150;
-            // 
-            // igazolvany
-            // 
-            this.igazolvany.HeaderText = "Igazolvány";
-            this.igazolvany.Name = "igazolvany";
-            this.igazolvany.ReadOnly = true;
-            this.igazolvany.Width = 150;
-            // 
             // Form
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(806, 404);
             this.Controls.Add(this.panel1);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox6);
-            this.Controls.Add(this.textBox5);
-            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.btmod);
+            this.Controls.Add(this.txutca);
+            this.Controls.Add(this.txvaros);
+            this.Controls.Add(this.txirszam);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.dtszuletett);
             this.Controls.Add(this.label5);
@@ -286,10 +292,10 @@
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.DateTimePicker dtszuletett;
         private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.TextBox textBox5;
-        private System.Windows.Forms.TextBox textBox6;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txirszam;
+        private System.Windows.Forms.TextBox txvaros;
+        private System.Windows.Forms.TextBox txutca;
+        private System.Windows.Forms.Button btmod;
         private System.Windows.Forms.DataGridView dgadatok;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.DataGridViewTextBoxColumn nev;
--- Form1.cs
+++ Form1.cs
@@ -15,6 +15,8 @@ namespace egyszerutanulok_20241205
     {
         List<Tanulok> lista = new List<Tanulok>();
         string elsosor;
+        string igazolvanyszam;
+        int index = 0;
         public Form()
         {
             InitializeComponent();
@@ -36,13 +38,9 @@ namespace egyszerutanulok_20241205
 
             for(int i = 0; i < lista.Count; i++)
             {
-                if (cbtelepules.Items.Contains(lista[i]))
+                if (!cbtelepules.Items.Contains(lista[i].Varos))
                 {
-                    return;
-                }
-                else
-                {
-                cbtelepules.Items.Add(lista[i].Varos);
+                    cbtelepules.Items.Add(lista[i].Varos);
                 }
             }
             
@@ -58,6 +56,128 @@ namespace egyszerutanulok_20241205
             if (rbigszam.Checked)
             {
                 txigszam.Enabled = true;
+            }
+        }
+
+        private void txigszam_TextChanged(object sender, EventArgs e)
+        {
+            //if(txigszam.Text.Length > 0)
+            //{
+            //    try
+            //    {
+            //         int.Parse(txigszam.Text);
+            //    }
+            //    catch
+            //    {
+            //        MessageBox.Show("Nem számot adtál meg!", "Hiba", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            //        txigszam.Clear();
+            //        txigszam.Focus();
+            //    }
+            //}
+        }
+
+        private void txnev_TextChanged(object sender, EventArgs e)
+        {
+            //if(txnev.Text.Length > 0)
+            //{
+            //    try
+            //    {
+            //        Convert.ToString(txnev.Text);
+            //    }
+            //    catch
+            //    {
+            //        MessageBox.Show("Nem betűt adtál meg!","Hiba",MessageBoxButtons.OK, MessageBoxIcon.Error);
+            //        txnev.Clear();
+            //        txnev.Focus();
+            //    }
+            //}
+        }
+
+        private void cbtelepules_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            dgadatok.Rows.Clear();
+            for (int i = 0; i<lista.Count; i++)
+            {
+                if (lista[i].Varos == cbtelepules.Text)
+                {
+                    dgadatok.Rows.Add(lista[i].Nev, lista[i].Igszam);
+                }
+            }
+        }
+
+        private void dgadatok_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            //DataGridViewRow sor = dgadatok.Rows[e.RowIndex];
+            //igazolvanyszam=sor.Cells["igazolvany"].Value.ToString();
+            //txigszam.Text = igazolvanyszam;
+
+        }
+
+        private void dgadatok_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            DataGridViewRow sor = dgadatok.Rows[e.RowIndex];
+            igazolvanyszam=sor.Cells["igazolvany"].Value.ToString();
+            txigszam.Text = igazolvanyszam;
+            while(lista[index].Igszam != igazolvanyszam)
+            {
+                index++;
+            }
+            txnev.Text = lista[index].Nev;
+            txosztaly.Text = lista[index].Osztaly;
+            txigszam.Text = lista[index].Igszam;
+            txirszam.Text = lista[index].Irszam;
+            txirszam.Text = lista[index].Varos;
+            txvaros.Text = lista[index].Utca;
+            dtszuletett.Value = lista[index].Szulido;
+        }
+
+        private void btmod_Click(object sender, EventArgs e)
+        {
+            if(txigszam.TextLength == 0)
+            {
+                MessageBox.Show("Adja meg az igazolványszámot!", "HIBA", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txigszam.Clear();
+                txigszam.Focus();
+            }
+            else if(txosztaly.Text.Length == 0)
+            {
+                MessageBox.Show("Adja meg az osztályát!", "HIBA", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txosztaly.Clear();
+                txosztaly.Focus();
+            }
+            else if (txnev.Text.Length == 0)
+            {
+                MessageBox.Show("Adja meg a nevét!", "HIBA", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txnev.Clear();
+                txnev.Focus();
+            }
+            else if (txirszam.Text.Length == 0)
+            {
+                MessageBox.Show("Adja meg az irányítószámát!", "HIBA", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txirszam.Clear();
+                txirszam.Focus();
+            }
+            else if (txvaros.Text.Length == 0)
+            {
+                MessageBox.Show("Adja meg a városát!", "HIBA", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txvaros.Clear();
+                txvaros.Focus();
+            }
+            else if (txutca.Text.Length == 0)
+            {
+                MessageBox.Show("Adja meg az utcáját!", "HIBA", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txutca.Clear();
+                txutca.Focus();
+            }
+            else
+            {
+                lista[index].Osztaly = txosztaly.Text;
+                lista[index].Igszam = txigszam.Text;
+                lista[index].Varos = txvaros.Text;
+                lista[index].Irszam = txirszam.Text;
+                lista[index].Nev = txnev.Text;
+                lista[index].Utca = txutca.Text;
+                lista[index].Szulido =
             }
         }
     }
